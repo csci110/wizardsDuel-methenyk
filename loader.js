644f0ac8e1ea3a5c52f9c9ded904714498015c83
Katyanna Metheny <methenyk@dewv.edu>
2018-09-05 16:50:59 +0000

    Updates via sync script

---
+++ loader.js
@@ -0,0 +1,7 @@
+import {game} from "./sgc/sgc.js";
+
+game.preloadImage("floor.png");
+game.preloadImage("marcusSheet.png", 48, 48);
+game.preloadImage("marcusSpellSheet.png", 384, 48);
+game.preloadImage("strangerSheet.png", 48, 48);
+game.preloadImage("strangerSpellSheet.png", 96, 48);
--- wizardsDuel.js
+++ wizardsDuel.js
@@ -1,1 +1,60 @@
+import {game, Sprite} from "./sgc/sgc.js";
 
+game.setBackground("floor.png");
+
+
+class playerWizard extends Sprite{
+    constructor() {
+        super();
+        this.name = "Marcus the Wizard";
+        this.width = 48;
+        this.height = 48;
+        this.setImage ("marcusSheet.png");
+        this.x = this.width;
+        this.y = this.y;
+        this.defineAnimation("down", 6, 8);
+        this.defineAnimation("up",0 ,2);
+        this.defineAnimation ("right", 3, 5);
+        this.defineAnimation("left", 9, 11);
+        this.speedWhenWalking = 100;
+    }
+    
+    handleDownArrowKey() {
+        this.playAnimation("down");
+        this.speed = this.speedWhenWalking;
+        this.angle = 270;
+    }
+    
+    handleUpArrowKey() {
+        this.playAnimation("up");
+        this.speed = this.speedWhenWalking;
+        this.angle = 90;
+    }
+    
+    handleRightArrowKey(){
+        this.playAnimation("right");
+        this.speed = this.speedWhenWalking;
+        this.angle = 360;
+    }
+    
+    handleLeftArrowKey(){
+        this.playAnimation("left");
+        this.speed = this.speedWhenWalking;
+        this.angle = 180;
+    }
+    
+    handleGameLoop(){ // Keep Marcus in the display area
+        this.y = Math.max(0, this.y); //Keeps the y above 0 
+        this.y = Math.min(552, this.y); //Keeps the y below 552
+        this.speed = 0;
+    }
+
+}
+
+let marcus = new playerWizard();
+marcus.name = "Marcus the Wizard";
+marcus.width = 48;
+marcus.height = 48;
+marcus.setImage("marcusSheet.png");
+marcus.x = this.width;
+marcus.y = this.y;
